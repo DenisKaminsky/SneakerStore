1fc291952a80e8ecc86488446f9c170e1d7d91d0
DenisKaminsky <36419821+DenisKaminsky@users.noreply.github.com>
2019-04-16 23:35:56 +0300

    add assets
    add font "Planet Kosmos" and images routes

---
+++ assets/products_uri.js
@@ -0,0 +1,104 @@
+const images = {
+    _1: {//Balenciaga Triple S
+        _1: "https://cdn-images.farfetch-contents.com/13/40/43/20/13404320_16094004_",
+        _2: "https://cdn-images.farfetch-contents.com/13/40/43/20/13404320_16094011_",
+        _3: "https://cdn-images.farfetch-contents.com/13/40/43/20/13404320_16094026_"
+    },
+    _2:{//Prada CloudBust
+        _1:"https://cdn-images.farfetch-contents.com/13/59/98/46/13599846_16162577_",
+        _2:"https://cdn-images.farfetch-contents.com/13/59/98/46/13599846_16162582_",
+        _3:"https://cdn-images.farfetch-contents.com/13/59/98/46/13599846_16162592_"
+    },
+    _3:{//Versace Chain Reaction
+        _1:"https://cdn-images.farfetch-contents.com/13/02/59/23/13025923_13855009_",
+        _2:"https://cdn-images.farfetch-contents.com/13/02/59/23/13025923_13855011_",
+        _3:"https://cdn-images.farfetch-contents.com/13/02/59/23/13025923_13855013_"
+    },
+    _4:{//Gucci FlashTreck
+        _1:"https://cdn-images.farfetch-contents.com/13/43/39/46/13433946_15404395_",
+        _2:"https://cdn-images.farfetch-contents.com/13/43/39/46/13433946_15404397_",
+        _3:"https://cdn-images.farfetch-contents.com/13/43/39/46/13433946_15404398_"
+    },
+    _6: {//Balenciaga Track
+        _1: "https://cdn-images.farfetch-contents.com/13/27/38/10/13273810_15346002_",
+        _2: "https://cdn-images.farfetch-contents.com/13/27/38/10/13273810_15346004_",
+        _3: "https://cdn-images.farfetch-contents.com/13/27/38/10/13273810_15346007_"
+    },
+    _5:{//Maison Margiela Fusion
+        _1: "https://cdn-images.farfetch-contents.com/13/55/80/48/13558048_16361482_",
+        _2: "https://cdn-images.farfetch-contents.com/13/55/80/48/13558048_16361481_",
+        _3: "https://cdn-images.farfetch-contents.com/13/55/80/48/13558048_16361480_"
+    },
+    _7:{//CK STRIKE
+        _1: "https://cdn-images.farfetch-contents.com/13/34/37/50/13343750_16764439_",
+        _2: "https://cdn-images.farfetch-contents.com/13/34/37/50/13343750_16764442_",
+        _3: "https://cdn-images.farfetch-contents.com/13/34/37/50/13343750_16764444_"
+    },
+    _8:{//Jordan x Spike Lee Spizike
+        _1: "https://cdn-images.farfetch-contents.com/13/47/60/29/13476029_16692166_",
+        _2: "https://cdn-images.farfetch-contents.com/13/47/60/29/13476029_16692174_",
+        _3: "https://cdn-images.farfetch-contents.com/13/47/60/29/13476029_16692195_"
+    },
+    _9:{//Gucci Rython
+        _1:"https://cdn-images.farfetch-contents.com/13/71/31/20/13713120_16547336_",
+        _2:"https://cdn-images.farfetch-contents.com/13/71/31/20/13713120_16547340_",
+        _3:"https://cdn-images.farfetch-contents.com/13/71/31/20/13713120_16547339_"
+    },
+    _10:{//AF-1
+        _1:"https://cdn-images.farfetch-contents.com/13/60/36/13/13603613_16360619_",
+        _2:"https://cdn-images.farfetch-contents.com/13/60/36/13/13603613_16360621_",
+        _3:"https://cdn-images.farfetch-contents.com/13/60/36/13/13603613_16360624_"
+    },
+    _11:{//Fendi
+        _1:"https://cdn-images.farfetch-contents.com/13/48/01/03/13480103_15943807_",
+        _2:"https://cdn-images.farfetch-contents.com/13/48/01/03/13480103_15943808_",
+        _3:"https://cdn-images.farfetch-contents.com/13/48/01/03/13480103_15943810_"
+    },
+    _12:{//350 v2 static
+        _1:"https://cdn-images.farfetch-contents.com/13/67/88/19/13678819_16498811_",
+        _2:"https://cdn-images.farfetch-contents.com/13/67/88/19/13678819_16498812_",
+        _3:"https://cdn-images.farfetch-contents.com/13/67/88/19/13678819_16498813_"
+    },
+    _13:{//Gucci Rython Web
+        _1:"https://cdn-images.farfetch-contents.com/12/96/48/78/12964878_13555637_",
+        _2:"https://cdn-images.farfetch-contents.com/12/96/48/78/12964878_13555641_",
+        _3:"https://cdn-images.farfetch-contents.com/12/96/48/78/12964878_13555644_"
+    },
+    _14:{//Air Jordan 12 RTR Michigan
+        _1:"https://cdn-images.farfetch-contents.com/13/15/76/05/13157605_14718469_",
+        _2:"https://cdn-images.farfetch-contents.com/13/15/76/05/13157605_14718470_",
+        _3:"https://cdn-images.farfetch-contents.com/13/15/76/05/13157605_14718471_"
+    },
+    _15:{//CK Cander 7
+        _1:"https://cdn-images.farfetch-contents.com/13/34/37/47/13343747_16137763_",
+        _2:"https://cdn-images.farfetch-contents.com/13/34/37/47/13343747_16137764_",
+        _3:"https://cdn-images.farfetch-contents.com/13/34/37/47/13343747_16137766_"
+    },
+    _16:{//700 v2 static
+        _1:"https://cdn-images.farfetch-contents.com/13/67/89/19/13678919_16498815_",
+        _2:"https://cdn-images.farfetch-contents.com/13/67/89/19/13678919_16498816_",
+        _3:"https://cdn-images.farfetch-contents.com/13/67/89/19/13678919_16498817_"
+    },
+    _17: {//Balenciaga Triple S
+        _1: "https://cdn-images.farfetch-contents.com/12/96/70/13/12967013_14412143_",
+        _2: "https://cdn-images.farfetch-contents.com/12/96/70/13/12967013_14412144_",
+        _3: "https://cdn-images.farfetch-contents.com/12/96/70/13/12967013_14412146_"
+    },
+    _18:{//Nike x Off White
+        _1: "https://cdn-images.farfetch-contents.com/13/15/76/04/13157604_14718464_",
+        _2: "https://cdn-images.farfetch-contents.com/13/15/76/04/13157604_14718465_",
+        _3: "https://cdn-images.farfetch-contents.com/13/15/76/04/13157604_14718466_"
+    },
+    _19:{//Maison Margiela Antisanal
+        _1: "https://cdn-images.farfetch-contents.com/13/40/01/08/13400108_16514923_",
+        _2: "https://cdn-images.farfetch-contents.com/13/40/01/08/13400108_16514926_",
+        _3: "https://cdn-images.farfetch-contents.com/13/40/01/08/13400108_16514930_"
+    },
+    _20:{//Burberry
+        _1: "https://cdn-images.farfetch-contents.com/13/54/86/74/13548674_16006750_",
+        _2: "https://cdn-images.farfetch-contents.com/13/54/86/74/13548674_16006751_",
+        _3: "https://cdn-images.farfetch-contents.com/13/54/86/74/13548674_16006753_"
+    }
+};
+
+export default images;
